79d00cf727d2720cc83370ab4925515d3022622d
Manan Joshi <154045858+Mananjoshii@users.noreply.github.com>
2025-02-08 11:46:01 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,1 @@
-import pg from 'pg';
 
-const { Pool } = pg;
-
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'manan',
-  password: '1234',
-  port: 5432,
-});
-
-export default {
-  query: (text, params) => pool.query(text, params),
-};
